2301a7f7370bf68d4aab2e52f640b5a0fba11a82
Laia Navalon <laianavalon@gmail.com>
2024-01-29 14:28:12 +0100

    Create movingArrows class + create arrows at random with interval

--- js/index.js
+++ js/index.js
@@ -112,8 +112,33 @@ class rightStaticArrow {
         const boardElm = document.getElementById("board");
         boardElm.appendChild(this.domElm);
     }
+
 }
 
+class movingArrows {
+    constructor() {
+        this.width = 90;
+        this.height = 90;
+        this.positionY = 0;
+    }
+
+    createDomElement(direction, position){
+        // step1: create the element
+        this.domElm = document.createElement("img");
+
+        // step2: add content or modify
+        this.domElm.setAttribute("src", direction);
+        this.domElm.setAttribute("class", "static-arrows")
+        this.domElm.style.width = this.width + "px"
+        this.domElm.style.height = this.height + "px"
+        this.domElm.style.left = position + "px";
+        this.domElm.style.bottom = this.positionY + "px";
+        
+        //step3: append to the dom: `parentElm.appendChild()`
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElm);
+    }
+}
 
 const staticArrows = [];
 const leftArrow = new leftStaticArrow();
@@ -123,3 +148,35 @@ const rightArrow = new rightStaticArrow();
 
 staticArrows.push(leftArrow, downArrow, upArrow, rightArrow)
 console.log(staticArrows)
+
+arrowDirections = [
+    {
+        direction: "./images/arrows/moving-left.png",
+        position: leftArrow.positionX
+    },
+    {
+        direction: "./images/arrows/moving-down.png",
+        position: downArrow.positionX,
+    },
+    {
+        direction: "./images/arrows/moving-up.png",
+        position: upArrow.positionX,
+    },
+    {
+        direction: "./images/arrows/moving-right.png",
+        position: rightArrow.positionX,
+    }
+]
+
+const randomArrows = [];
+
+setInterval(() => {
+    const randomIndex = Math.floor(Math.random() * arrowDirections.length)
+        
+    const randomDirection = arrowDirections[randomIndex].direction;
+    const randomPosition = arrowDirections[randomIndex].position;
+    
+    const newArrow = new movingArrows();
+    newArrow.createDomElement(randomDirection,randomPosition)
+    randomArrows.push(newArrow);
+}, 3000);
