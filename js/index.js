2a8e22e27629fa2f1c4635e90c599158cd10b046
Laia Navalon <laianavalon@gmail.com>
2024-01-31 18:50:39 +0100

    Add difficulty radio buttons to index.html

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 const mainDiv = document.getElementById("intro")
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 4; i++) {
     const div = document.createElement("div")
     div.setAttribute("id", `div${i}`)
     mainDiv.appendChild(div)
@@ -25,7 +25,6 @@ const introText = document.createElement("h2");
 introText.setAttribute("id", "intro-header")
 introText.innerText = `はじめまして, TOMOEです`;
 introText.style.color = "white"
-// introText.style.backgroundColor = "white";
 introText.style.padding = "5px 10px 0px 10px";
 
 textBox.appendChild(introText)
@@ -37,16 +36,50 @@ One of the arts she has to master is
 the Mai, the Japanese traditional
 dance with a fan.
 
-Can you help her practice?`;
+Can you help her practice?
+But first, choose:`;
 instructions.style.textAlign = "center";
 instructions.style.width = "300px"
 instructions.style.color = "white"
-// instructions.style.backgroundColor = "white";
 instructions.style.padding = "0px 10px 15px 10px";
 instructions.style.left = "100px";
 instructions.style.margin = "0px"
 
 textBox.appendChild(instructions)
+
+
+/////// RADIO BUTTONS ///////
+
+const radioButtons = []
+
+const radioDiv = document.getElementById("div2")
+
+radioDiv.style.position = "absolute";
+radioDiv.style.left = "55px";
+radioDiv.style.bottom = "170px";
+
+const levels = [ "Easy", "Medium", "Difficult"]
+
+levels.forEach((elm, i) => {
+    const radioButton = document.createElement("input");
+    const label = document.createElement("label");
+    radioButton.setAttribute("type", "radio");
+    radioButton.setAttribute("name", "levels");
+    radioButton.setAttribute("value", elm);
+    radioButton.setAttribute("class", "radio-button")
+    radioButton.style.backgroundColor = "red";
+    label.style.padding = "5px";
+    label.style.fontSize = "20px"
+    label.innerText = elm;
+
+    if (i === 0) {
+        radioButton.setAttribute("checked", "checked");
+    }
+
+    radioButtons.push(radioButton);
+    radioDiv.appendChild(radioButton);
+    radioDiv.appendChild(label);
+});
 
 
 
@@ -58,13 +91,13 @@ const buttonMarginTop = 40;
 const buttonHref = "game.html";
 
 const playButton = new Button(buttonMarginTop);
-const button = playButton.createButtonElement(playButtonText, "absolute", buttonHref, "div2", "65", "80")
+const button = playButton.createButtonElement(playButtonText, "absolute", buttonHref, "div3", "65", "70")
 
 
 
 ////// GEISHA IMAGE ///////
 
-const introImg = document.getElementById("div3");
+const introImg = document.getElementById("div4");
 
 geishaImg = document.createElement("img");
 geishaImg.setAttribute("src", "./images/geisha.png");
